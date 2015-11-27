fac996b5cd9cf63d4f0a472b7512fb915d7fcb21
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-27 13:26:43 +0900

    Write to authorized_keys

--- main.go
+++ main.go
@@ -2,24 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/ogier/pflag"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Usage: gh-ssh-pubkey [username, ...]")
+	var write bool
+	pflag.BoolVarP(&write, "write", "w", false, "write to ~/.ssh/authorized_keys")
+	pflag.Parse()
+
+	if len(pflag.Args()) == 0 {
+		fmt.Fprintln(os.Stderr, "Usage: gh-ssh-pubkey [-w] username [, username2...]")
 		os.Exit(1)
 	}
 
-	for _, name := range os.Args[1:] {
+	out, err := Out(write)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	for _, name := range pflag.Args() {
 		keys, err := GetSSHKey(name)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("# from https://github.com/%s\n", name)
-		fmt.Println(keys)
+		fmt.Fprintf(out, "\n# from https://github.com/%s\n", name)
+		fmt.Fprint(out, keys)
 	}
 }
 
@@ -31,4 +45,16 @@ func GetSSHKey(name string) (string, error) {
 	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
 	return string(b), err
+}
+
+func Out(write bool) (io.WriteCloser, error) {
+	if !write {
+		return os.Stdout, nil
+	}
+
+	path, err := homedir.Expand("~/.ssh/authorized_keys")
+	if err != nil {
+		return nil, err
+	}
+	return os.OpenFile(path, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0600)
 }
